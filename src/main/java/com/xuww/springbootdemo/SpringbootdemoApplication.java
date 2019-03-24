c68e7a8c5391d6be522b5faa775c2d4dc462fec9
xuww00 <xuww00@foxmail.com>
2019-03-24 14:13:22 +0800

    wenjian

--- src/main/java/com/xuww/springbootdemo/SpringbootdemoApplication.java
+++ src/main/java/com/xuww/springbootdemo/SpringbootdemoApplication.java
@@ -3,6 +3,8 @@ package com.xuww.springbootdemo;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.data.mongo.MongoDataAutoConfiguration;
+import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
 
 @SpringBootApplication()
 @MapperScan({"com/xuww/springbootdemo/entity/mapper"})
